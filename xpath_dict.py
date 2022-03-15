7e926b2ebd8bd20c458c8db9c094027ec5f1a28b
Welly Huang <welly.huang@ni.com>
2022-03-15 17:10:59 +0800

    add the xpath_dict dependencies; update the name of the log to 'histoy'

---
+++ xpath_dict.py
@@ -0,0 +1,23 @@
+xpath_dict = {
+    "送出" : '//*[@id="CmnSendBtn"]',
+    "關閉" : '//*[@id="mdl_btn_close"]',
+    "存檔" : '//*[@id="mdl_btn_save"]',
+    "申請" : '//*[@id="MainContent_btnSave"]',
+    "登入" : '//*[@id="con_one_1"]/div[2]/a[1]',
+    "登入web" : '//*[@id="loginBtn"]',
+    "人資系統" : '//*[@id="hr"]',
+    "考勤補登申請單" : '/html/body/div/div[2]/div[1]/div/ul/li[3]/ul/li[3]/a',
+    "小時" : '//*[@id="MainContent_drpHour_chosen"]',
+    "小時按鈕" : '//*[@id="MainContent_drpHour_chosen"]/div/div/input',
+    "分鐘" : '//*[@id="MainContent_drpMinute_chosen"]',
+    "分鐘按鈕" : '//*[@id="MainContent_drpMinute_chosen"]/div/div/input',
+    "卡別" : '//*[@id="label-switch"]/div',
+    "卡別按鈕" : '//*[@id="MainContent_chbs_type"]',
+    "月曆按鈕" : '//*[@id="MainContent_txtYMD"]',
+    "月曆" : '//*[@id="_my97DP"]/iframe',
+    "月份按鈕" : '//*[@id="dpTitle"]/div[3]/input',
+    "公司" : '//*[@id="company_id"]',
+    "帳號" : '//*[@id="login_name"]',
+    "密碼" : '//*[@id="password"]'
+}
+
